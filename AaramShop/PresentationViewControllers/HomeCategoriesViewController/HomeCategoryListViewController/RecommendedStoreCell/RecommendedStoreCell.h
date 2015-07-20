3afc4080843f668203aa22446c747a56466b2314
priyanka kansal <priyanka@approutes.com>
2015-07-20 18:57:02 +0530

    updated by chetan

---
+++ AaramShop/PresentationViewControllers/HomeCategoriesViewController/HomeCategoryListViewController/RecommendedStoreCell/RecommendedStoreCell.h
@@ -0,0 +1,13 @@
+//
+//  RecommendedStoreCell.h
+//  AaramShop
+//
+//  Created by Approutes on 20/07/15.
+//  Copyright (c) 2015 Approutes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecommendedStoreCell : UITableViewCell
+
+@end
---
+++ AaramShop/PresentationViewControllers/HomeCategoriesViewController/HomeCategoryListViewController/RecommendedStoreCell/RecommendedStoreCell.m
@@ -0,0 +1,23 @@
+//
+//  RecommendedStoreCell.m
+//  AaramShop
+//
+//  Created by Approutes on 20/07/15.
+//  Copyright (c) 2015 Approutes. All rights reserved.
+//
+
+#import "RecommendedStoreCell.h"
+
+@implementation RecommendedStoreCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
