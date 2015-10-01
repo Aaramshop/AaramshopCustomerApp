3c3b46e425618c4e6039b01f33f6c3968e53e7aa
Shubhendu Shukla <shubhendu1988@gmail.com>
2015-10-01 12:00:08 +0530

    Arbab

--- AaramShop/PresentationViewControllers/HomeSecondViewController/HomeSecondViewController.m
+++ AaramShop/PresentationViewControllers/HomeSecondViewController/HomeSecondViewController.m
@@ -270,6 +270,10 @@
             [Utils showAlertView:kAlertTitle message:[responseObject objectForKey:kMessage] delegate:nil cancelButtonTitle:kAlertBtnOK otherButtonTitles:nil];
 
         }
+		dispatch_async(dispatch_get_main_queue(), ^{
+			[tblVwCategory reloadData];
+		});
+//		[tblVwCategory reloadData];
     }
     
     
@@ -475,7 +479,7 @@
         }
     }
 
-    [tblVwCategory reloadData];
+	
 }
 #pragma mark Navigation
 
